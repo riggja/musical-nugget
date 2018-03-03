1c1932ff8cb02b89049d95e6e77e3a51e8ba9c46
Matthew Ma <matthew.ma@wallawalla.edu>
2018-03-03 01:10:46 +0000

    autosave

--- test.cpp
+++ test.cpp
@@ -1,11 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+    
+    // sf::RectangleShape paddle(sf::Vector2f(50, 120));
+    // paddle.setFillColor(sf::Color::Green);
+    // paddle.setOutlineThickness(10);
+    // paddle.setOutlineColor(sf::Color(250, 150, 100));
+    // sf::RectangleShape ball(sf::Vector2f(50, 50));
+    // ball.setFillColor(sf::Color::Blue);
+    
+    sf::Texture paddleTexture;
+    if (!paddleTexture.loadFromFile("CharacterSprite.jpg")) {
+        cout << "mistake" << endl;
+    }
+    sf::Texture ballTexture;
+    if (!ballTexture.loadFromFile("BallSprite.jpg")) {
+        cout << "mistake" << endl;
+    }
+    
+    sf::Sprite paddle;
+    paddle.setTexture(paddleTexture);
+    
+    sf::Sprite ball;
+    ball.setTexture(ballTexture);
+    
     while (window.isOpen())
     {
         sf::Event event;
@@ -17,18 +39,27 @@ int main()
         
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
         {
-            // left key is pressed: move our character
-            shape.move(0, -1);
+            // up key is pressed: move our character
+            paddle.move(0, -1);
         }
         
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
         {
-            // left key is pressed: move our character
-            shape.move(0, 1);
+            // down key is pressed: move our character
+            paddle.move(0, 1);
+        }
+        
+        if (ball.getGlobalBounds().intersects(paddle.getGlobalBounds())) {
+            window.clear();
+            cout << "COLLISION" << endl;
+        } else {
+            window.display();
+            cout << "Not Collision" << endl;
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(paddle);
+        window.draw(ball);
         window.display();
     }
 
